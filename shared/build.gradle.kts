ca340c95c03e623ed6f5936463d1333e940faa90
Dive <72026841+0xDive@users.noreply.github.com>
2026-08-16 16:08:41 +0300

    refactor(push): share backend registration contract

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.jetbrains.compose)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 kotlin {
