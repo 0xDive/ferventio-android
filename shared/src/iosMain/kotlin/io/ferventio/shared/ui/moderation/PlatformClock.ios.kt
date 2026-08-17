65cfa059694bc6eea8df2e4c9866312c49dfda3a
Dive <72026841+0xDive@users.noreply.github.com>
2026-08-17 19:23:55 +0300

    refactor(moderation): remove iOS clock adapter

--- shared/src/iosMain/kotlin/io/ferventio/shared/ui/moderation/PlatformClock.ios.kt
+++
@@ -1,6 +0,0 @@
-package io.ferventio.shared.ui.moderation
-
-import platform.Foundation.NSDate
-
-internal actual fun currentEpochMillis(): Long =
-    (NSDate().timeIntervalSince1970 * 1_000.0).toLong()
