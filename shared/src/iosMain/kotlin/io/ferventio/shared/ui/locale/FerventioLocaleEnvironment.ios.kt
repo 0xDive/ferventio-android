30928bd7dd8f3c078c9317336d8c89046ccd7a12
Dive <72026841+0xDive@users.noreply.github.com>
2026-08-22 12:59:59 +0300

    Restore iOS system locale after overrides

--- shared/src/iosMain/kotlin/io/ferventio/shared/ui/locale/FerventioLocaleEnvironment.ios.kt
+++ shared/src/iosMain/kotlin/io/ferventio/shared/ui/locale/FerventioLocaleEnvironment.ios.kt
@@ -10,16 +10,18 @@ import platform.Foundation.NSUserDefaults
 @OptIn(InternalComposeUiApi::class)
 internal actual object LocalFerventioAppLocale {
     private const val LANGUAGE_KEY = "AppleLanguages"
-    private val defaultLanguage = NSLocale.preferredLanguages.first() as String
-    private val localLanguage = staticCompositionLocalOf { defaultLanguage }
+    private val localLanguage = staticCompositionLocalOf {
+        NSLocale.preferredLanguages.first() as String
+    }
 
     @Composable
     actual infix fun provides(value: String?): ProvidedValue<*> {
-        val language = value ?: defaultLanguage
-        if (value == null) {
+        val language = if (value == null) {
             NSUserDefaults.standardUserDefaults.removeObjectForKey(LANGUAGE_KEY)
+            NSLocale.preferredLanguages.first() as String
         } else {
-            NSUserDefaults.standardUserDefaults.setObject(arrayListOf(language), LANGUAGE_KEY)
+            NSUserDefaults.standardUserDefaults.setObject(arrayListOf(value), LANGUAGE_KEY)
+            value
         }
         return localLanguage.provides(language)
     }
